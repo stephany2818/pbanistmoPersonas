2018bbf498155e79d645529057b94aa5c0fac1aa
stephany2818 <66704159+stephany2818@users.noreply.github.com>
2020-07-13 10:09:56 -0500

    stephany2818
    Commit

--- src/main/java/com/banistmo/personas/certification/userinterfaces/RatePage.java
+++ src/main/java/com/banistmo/personas/certification/userinterfaces/RatePage.java
@@ -4,7 +4,8 @@ import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
-public class RatePage extends PageObject {
+public class RatePage extends PageObject
+{
     public static final Target LABEL_RATE = Target.the("Label Rate")
             .located(By.id("termino"));
 }
